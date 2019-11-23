47723349fe22e90123a6e3828630e9e9007183eb
stellaribas <stelladasilvaribas@gmail.com>
2019-11-23 18:26:37 -0300

    formulario de cadastro, login e denuncia

--- form.py
+++ form.py
@@ -1,15 +1,18 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField
+from wtforms import StringField, PasswordField, BooleanField, TextAreaField
 from wtforms.validators import DataRequired, Email
 
 class CadastroForm (FlaskForm):
     usuario = StringField('Usuario', validators=[DataRequired()])
-    senha = PasswordField("Senha", validators=[DataRequired])
+    senha = PasswordField("Senha", validators=[DataRequired()])
     email = StringField('Email', validators=[DataRequired(), Email()])
 
 
 class LoginForm(FlaskForm):
-    usuario = StringField("usuario", validators=[DataRequired()])
-    senha = PasswordField("senha", validators=[DataRequired])
-    lembrar_Senha = BooleanField("lembrar_senha")
-
+    usuario = StringField("Usuario", validators=[DataRequired()])
+    senha = PasswordField("Senha", validators=[DataRequired()])
+  
+class DenunciasForm(FlaskForm):
+    latitude = StringField('Latitude', validators=[DataRequired()])
+    longitude = StringField('Longitude', validators=[DataRequired()])
+    mensagem = TextAreaField('Mensagem', validators=[DataRequired()])
